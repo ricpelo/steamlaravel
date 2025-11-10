63fd8e446ca11db119e67a5f0c466ad567922cea
Ricardo Pérez <ricpelo@gmail.com>
2025-11-10 21:27:12 +0100

    Añado la ruta /hola

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hola', function (Request $request) {
+    $nombre = $request->query('nombre');
+    return "Hola, $nombre";
 });
