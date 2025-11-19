e327b67319f1ccbf03f4c6f1631b60712a13d807
Ricardo Pérez <ricpelo@gmail.com>
2025-11-19 18:16:04 +0100

    Meto un fillable

--- app/Models/Videojuego.php
+++ app/Models/Videojuego.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Videojuego extends Model
 {
+    protected $fillable = [
+        'nombre',
+        'precio',
+        'lanzamiento',
+        'desarrolladora_id',
+    ];
+
     public function desarrolladora(): BelongsTo
     {
         return $this->belongsTo(Desarrolladora::class);
