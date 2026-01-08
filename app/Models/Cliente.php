97ff1a1baa6283d4a84c4796215eb2fb6af45137
Ricardo Pérez <ricpelo@gmail.com>
2026-01-08 17:55:03 +0100

    Vuelos

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -20,5 +21,24 @@ class Cliente extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+
+        /*
+        Vuelo::withCount('reservas')
+            ->where('salida', '>', now())
+            ->having('count_reservas', '<', 'plazas')
+            ->get();
+
+        SELECT vuelos.*, COUNT(*) AS count_reservas
+        FROM vuelos JOIN reservas ON reservas.vuelo_id = vuelos.id
+        WHERE salida > CURRENT_DATE
+        GROUP BY vuelos.*
+        HAVING COUNT(*) < plazas;
+
+        $c = collect()->range(1, $v->plazas)->diff($v->reservas()->pluck('asiento'));
+
+        SELECT asiento
+        FROM reservas
+        WHERE vuelo_id = 5;
+        */
     }
 }
